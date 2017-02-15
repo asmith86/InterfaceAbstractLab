771307d4daf87e75c671f463cb6e862cef3659f8
asmith86 <asmith86@L11712042775.wctc.msft>
2017-02-14 19:39:40 -0600

    Started Interface/Abstract Lab

---
+++ src/lab1/ProgrammingCourse.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author asmith86
+ */
+public abstract class ProgrammingCourse {
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+    
+    
+
+    public final void setCourseName(String courseName) {
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
+
+    public final void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseNumber = courseNumber;
+    }
+
+    
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public abstract void setCredits(double credits); 
+        
+    
+    
+    
+}
